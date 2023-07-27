e790f069e227c7d9c74c88cc0f6ecf6c28ac4ce8
Kane Stoboi <kanestoboi@gmail.com>
2023-07-28 08:09:54 +1000

    Updating DFU package generation script to use bl settings variable

--- dfu_images/GenerateDFUPackage.bat
+++ dfu_images/GenerateDFUPackage.bat
@@ -3,13 +3,14 @@
 set build-configuration=Release
 set /A firmwareVersion=2
 set /A bootloaderVersion=1
+set /A blSettingsVersion=1
 
 set application-hex=../Output/%build-configuration%/Exe/TrailerLeveler.hex
 set soft-device-hex=../../nRF5_SDK_Current/components/softdevice/s140/hex/s140_nrf52_7.2.0_softdevice.hex
 set bootloader-hex=../../TrailerLeveler-Bootloader/Output/Release/Exe/TrailerLevelerSecureBootloader_s140_pca10056.hex
 
 echo Generating DFU Settings Page
-nrfutil.exe settings generate --family NRF52840 --application %application-hex% --application-version %firmwareVersion% --bootloader-version %bootloaderVersion% --bl-settings-version 1 bl_settings.hex
+nrfutil.exe settings generate --family NRF52840 --application %application-hex% --application-version %firmwareVersion% --bootloader-version %bootloaderVersion% --bl-settings-version %blSettingsVersion% bl_settings.hex
 
 echo Merging Bootloader, Soft Device, Settings, and Application
 mergehex --merge bl_settings.hex %bootloader-hex% %soft-device-hex% %application-hex% --output bl_sd_settings_app.hex
