8239e364387ce7e4c97fb571cfbbe7f82ae75a98
Kane Stoboi <kanestoboi@gmail.com>
2023-09-02 12:09:06 +1000

    incrementing minor version for firmware release

--- dfu_images/GenerateDFUPackage.bat
+++ dfu_images/GenerateDFUPackage.bat
@@ -6,7 +6,7 @@ emBuild -config "Release" -echo ../TrailerLeveler_pca10056_s140.emProject
 set build-configuration=Release
 
 set majorFirmwareVersion=0
-set minorFirmwareVersion=3
+set minorFirmwareVersion=4
 set patchFirmwareVersion=0
 
 @REM set the firmware version as a uint32 where each bytes represents the major, minor and patch versionsS
