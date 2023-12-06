9f211a01d39e224d4301212ecc1313012e43311e
Kane Stoboi <kanestoboi@gmail.com>
2023-12-06 20:01:23 +1000

    creating release 5.0

--- dfu_images/GenerateDFUPackage.bat
+++ dfu_images/GenerateDFUPackage.bat
@@ -6,8 +6,8 @@ emBuild -config "Release" -echo ../TrailerLeveler_pca10056_s140.emProject
 set build-configuration=Release
 
 set majorFirmwareVersion=0
-set minorFirmwareVersion=4
-set patchFirmwareVersion=2
+set minorFirmwareVersion=5
+set patchFirmwareVersion=0
 
 @REM set the firmware version as a uint32 where each bytes represents the major, minor and patch versionsS
 set /a firmwareVersion=(majorFirmwareVersion * 65536) + (minorFirmwareVersion * 256) + patchFirmwareVersion
